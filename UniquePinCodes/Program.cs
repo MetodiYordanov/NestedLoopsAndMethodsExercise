11db8f316fd651352c3b76ed26eab634bd862de9
metodi.yordanov <metodi.yordanov99@gmail.com>
2023-12-08 22:57:18 +0200

    Finished "UniquePinCodes" exercise by adding method that calculates if number is prime or not.

--- UniquePinCodes/Program.cs
+++ UniquePinCodes/Program.cs
@@ -8,7 +8,25 @@ for (int i = 2; i <= max1; i = i + 2)
     {
         for (int k = 2; k <= max3; k = k + 2)
         {
-
+            if (IsPrime(j))
+            {
+                Console.WriteLine($"{i}{j}{k}");
+            }
         }
     }
+}
+
+static bool IsPrime(int number)
+{
+    if (number <= 1) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+
+    var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i += 2)
+        if (number % i == 0)
+            return false;
+
+    return true;
 }
